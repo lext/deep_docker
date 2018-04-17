e794f3bab98c5d18204efb10192735e4e3853b39
lext <aleksei.tiulpin@protonmail.ch>
2018-04-17 15:37:41 +0200

    added scripts for devbox connection

---
+++ connect_devboxes_mac.sh
@@ -0,0 +1,7 @@
+for scr in $(ls /tmp/uscreens/S-$USER)
+do
+    screen -X -S $scr quit
+done
+screen -dmS ssh_tunnel && screen -S ssh_tunnel -X stuff "ssh -N -L 1231:localhost:1231 $1@$2 >/dev/null 2>&1\n"
+screen -dmS jupyter_tunnel && screen -S jupyter_tunnel -X stuff "ssh -N -L 1232:localhost:1232 $1@$2 >/dev/null 2>&1\n"
+screen -dmS tboard_tunnel && screen -S tboard_tunnel -X stuff "ssh -N -L 1233:localhost:1233 $1@$2 >/dev/null 2>&1\n"
