bc350173eeda09d4e7ecdece649e7ac6148bde85
Aleksei Tiulpin <aleksei.tiulpin@protonmail.ch>
2018-06-07 10:56:55 +0300

    fixed DNS for Oulu Uni network

--- run_docker.sh
+++ run_docker.sh
@@ -3,7 +3,7 @@ docker stop dl_container
 docker rm dl_container
 
 # Running the new instance
-nvidia-docker run --ipc=host -d --mount type=bind,src=$DATADIR,dst=/data -P -p 1231:22 -p 1232:8888 -p 1233:6006  --name dl_container deep_docker
+nvidia-docker run --dns 130.231.240.1 --ipc=host -d --mount type=bind,src=$DATADIR,dst=/data -P -p 1231:22 -p 1232:8888 -p 1233:6006  --name dl_container deep_docker
 
 docker exec -d dl_container sh /root/run_screens_docker.sh
 
