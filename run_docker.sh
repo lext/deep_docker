d0c9c236ac9bac56dbaa7bc73c063c6ac376edb2
Egor Panfilov <egor.v.panfilov@gmail.com>
2018-06-15 17:38:36 +0300

    Better handle the old container on start

--- run_docker.sh
+++ run_docker.sh
@@ -1,9 +1,20 @@
-# Stopping previously ran instances
-docker stop dl_container
-docker rm dl_container
+#!/bin/bash
 
-# Running the container
-nvidia-docker run --ipc=host -d --mount type=bind,src=$DATADIR,dst=/data -P -p 1231:22 -p 1232:8888 -p 1233:6006  --name dl_container deep_docker
+CONTAINER="dl_container"
 
-docker exec -d dl_container sh /root/run_screens_docker.sh
+# Stop and kill the container if it is running
+if [ "$(docker ps -q -f name=$CONTAINER)" ]; then
+    echo "Terminating the container"
+    docker stop $CONTAINER
+fi
+if [ "$(docker ps -aq -f status=exited -f name=$CONTAINER)" ]; then
+    echo "Deleting the container"
+    docker rm $CONTAINER
+fi
 
+# Run the container
+nvidia-docker run --ipc=host -d --mount type=bind,src=$DATADIR,dst=/data \
+              -P -p 1231:22 -p 1232:8888 -p 1233:6006 \
+              --name $CONTAINER deep_docker
+
+docker exec -d $CONTAINER sh /root/run_screens_docker.sh
